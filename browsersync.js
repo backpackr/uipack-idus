78f09a1e1fc04799a1eb67b1a9ce8b0a55afd598
7ylee <taeyouklee@gmail.com>
2018-08-31 14:23:30 +0900

    browsersync options

--- browsersync.js
+++ browsersync.js
@@ -8,6 +8,8 @@ const browserSync = require('browser-sync');
  * Run Browsersync with server config
  */
 browserSync({
+    ui: false,
+    notify: false,
     server: 'dev',
     files: ['dev/*.html', 'dev/*.css', 'dev/*js']
 });
